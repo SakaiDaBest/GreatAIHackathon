a0b8d4710d69f31dcf75364f9f8bce76ffdf0c08
WeiJun <wong.weijun923@gmail.com>
2025-09-20 13:16:15 +0800

    edited app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 async function checkNews() {
   const userInput = document.getElementById("news-input").value;
 
-  const response = await fetch("https://<your-api-id>.execute-api.ap-southeast-2.amazonaws.com/", {
+  const response = await fetch("https://gj2322u.execute-api.ap-southeast-2.amazonaws.com/", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ text: userInput }),
